0178f9063e0b2656b2619179c6b908d3a72afc4b
Kyle Pratuch & Alexandra Brown <student@epicodus.com>
2015-08-12 14:09:40 -0700

    add twig templates for RPS game frontend.

--- app/app.php
+++ app/app.php
@@ -3,18 +3,20 @@
     require_once __DIR__.'/../src/RockPaperScissors.php';
 
     $app = new Silex\Application();
+    $app['debug']=true;
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
         'twig.path' => __DIR__.'/../views'
     ));
 
-    $app->post("/", function() use ($app) {
-        $game = new RockPaperScissors ($_POST[''])
+    $app->get("/", function() use ($app) {
         return $app['twig']->render('play_one.html.twig');
     });
 
-    $app->post("/player_two", function() use ($app) {
-        return $app['twig']->render('play_two.html.twig');
+    $app->get("/winners", function() use ($app) {
+        $winner = new RockPaperScissors;
+        $the_winner = $winner->playGame($_GET['player1'], $_GET['player2']);
+        return $app['twig']->render('winners.html.twig', array('output' => $the_winner));
     });
 
     return $app;
